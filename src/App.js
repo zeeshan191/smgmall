1a9cd1a284c2d50f31370067eecb7d827ad8bea7
zeeshan191 <80030867+zeeshan191@users.noreply.github.com>
2021-09-03 20:49:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
+import Demo from './Demo';
+import Navbar1 from './Navbar1';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ <>
+ <Navbar1/>
+ <Router>
+ <Switch>
+ <Route path="/Demo" exact component={Demo} />
+ </Switch>
+ </Router>
+ </>
   );
 }
 
---
+++ src/Demo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Demo() {
+    return (
+        <div>
+           <img src='logo.png' width='250' height='200' alt="thumb"/>
+        </div>
+    )
+}
+
+export default Demo
